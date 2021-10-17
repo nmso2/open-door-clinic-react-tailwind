14375255d2a5f05cf5a314d233804474a23a6711
nmso2 <nmsubho@gmail.com>
2021-10-18 01:54:05 +0600

    installed necessary packages and added header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import "tailwindcss/tailwind.css"
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Header from './pages/Shared/Header/Header';
+import AboutUs from './pages/AboutUs/AboutUs';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <Header></Header>
+        <Switch>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route path="/home">
+            <Home></Home>
+          </Route>
+          <Route path="/about">
+            <AboutUs></AboutUs>
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
