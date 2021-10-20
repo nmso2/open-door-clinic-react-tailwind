b8c0e531826150239fb0561a60edf19a474251b6
nmso2 <nmsubho@gmail.com>
2021-10-20 18:18:50 +0600

    update

--- src/pages/Doctors/Doctor/Doctor.js
+++ src/pages/Doctors/Doctor/Doctor.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Doctor = (props) => {
-    const { name, address, img, phone, workday } = props.doctor;
+    const { name, address, img, phone, workday, speciality } = props.doctor;
     console.log(props.doctor);
     return (
         <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
@@ -9,15 +9,20 @@ const Doctor = (props) => {
 
                 <img alt="Placeholder" className="block h-auto w-full" src={img} />
 
-                <header className="leading-tight p-2 md:p-4">
-                    <h1 className="text-3xl">{name}</h1>
-                </header>
+                <div>
+                    <header className="leading-tight p-2 md:p-4">
+                        <h1 className="text-3xl">{name}</h1>
+                    </header>
+                    <body>
+                        <h3 className="text-2xl">{speciality}</h3>
+                    </body>
 
-                <footer className="leading-none p-2 md:p-4">
-                    <p className="ml-2 text-lg">{workday}</p>
-                    <p className="ml-2 text-lg">{phone}</p>
-                    <p className="ml-2 text-lg">{address}</p>
-                </footer>
+                    <footer className="leading-none p-2 md:p-4">
+                        <p className="ml-2 text-lg">{workday}</p>
+                        <p className="ml-2 text-lg">{phone}</p>
+                        <p className="ml-2 text-lg">{address}</p>
+                    </footer>
+                </div>
             </article>
         </div>
     );
